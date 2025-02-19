f6735f3cff194689481113ec4c4d0f931ec02b72
ADFAST LTDA <143732424+adfastltda@users.noreply.github.com>
2025-02-19 12:47:29 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@
 echo "Updating and upgrading system packages..."
 sudo apt update && sudo apt upgrade -y
 #OUTROS
-sudo apt install libsquashfuse0 squashfuse fuse snapd-y
+sudo apt install libsquashfuse0 squashfuse fuse snapd -y
 
 # Add Windsurf repository and install Windsurf
 echo "Adding Windsurf repository..."
